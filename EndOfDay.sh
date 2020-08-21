0011424fffbf459a45591ca83ff36d04e9c0abe0
Moses Troyer <troyerms@gmail.com>
2020-08-21 17:42:42 -0500

    fix cron

--- EndOfDay.sh
+++ EndOfDay.sh
@@ -1,4 +1,4 @@
-# Quits certain apps and mutes volume at the end of the work day in OSX. Set up with cron: crontab -e => * 17 * * * ~./EndOfDay.sh
+# Quits certain apps and mutes volume at the end of the work day in OSX. Set up with cron: crontab -e => 0 17 * * * cd /Users/troyerms/code/AutomationScripts && ./EndOfDay.sh
 
 osascript -e 'quit app "Discord"'
 osascript -e 'quit app "Hangouts Chat"'
